ff36018d05bdbcc51424a377eb7fcd3aa8b96017
DanielleColucci <ddcolucci99@gmail.com>
2023-01-03 18:05:45 -0500

    import Express and create Express app

---
+++ server.js
@@ -0,0 +1,18 @@
+// import modules 
+
+import express from 'express'
+
+// create Express app
+
+const app = express()
+
+// Configure the app 
+
+
+
+// Mount routes 
+
+
+
+// Tell the app to listen on port 3000 
+
