6cc93be84c24c49f5d44ddb96f243511c2639f1b
DanielleColucci <ddcolucci99@gmail.com>
2023-01-03 18:19:32 -0500

    add book-data file, create and export books data

---
+++ data/book-data.js
@@ -0,0 +1,9 @@
+const books = [
+  {title: 'Lord of the Flies', read: true, _id: 123456},
+  {title: 'Pride and Prejudice', read: false, _id: 123457},
+  {title: 'Frankenstein', read: true, _id: 123458}
+]
+
+export {
+  books
+}
